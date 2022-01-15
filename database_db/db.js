6f72b1b65e58be5fe6a21c4bbfb64491e5cb7b74
“Cynthia <xin2604@gmail.com>
2022-01-14 18:15:59 -0800

    create database connection

---
+++ database_db/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const connection = {
+  host: 'localhost',
+  user: process.env.PG_USER,
+  password: '',
+  database: 'questions',
+  port: 5432
+};
+
+const pool = new Pool(connection);
+pool.connect()
+  .then((response) => {
+    console.log('connect to database')
+  })
+  .catch((err) => console.error(err));
+
+module.exports = pool;
