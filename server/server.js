b2db425aef915fb3586c2ec44db52085bad529cd
“Cynthia <xin2604@gmail.com>
2022-01-18 09:51:56 -0800

    connect to client

--- server/server.js
+++ server/server.js
@@ -9,8 +9,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../../FEC-HRLAX48/dist')));
 
 app.use('/api', router);
 
