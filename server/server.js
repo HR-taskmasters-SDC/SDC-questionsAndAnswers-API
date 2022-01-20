c152ed77b951bb031a135d3daa5d5c69d5ebb058
“Cynthia <xin2604@gmail.com>
2022-01-20 10:05:52 -0800

    cleanup

--- server/server.js
+++ server/server.js
@@ -2,16 +2,13 @@ const express = require('express');
 const path = require('path');
 const router = require('./router.js');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../../FEC-HRLAX48/dist')));
 
 app.use('/api', router);
 
