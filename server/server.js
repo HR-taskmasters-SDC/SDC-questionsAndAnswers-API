4980b8db242d7fb806d9e20764f600b52c389d8a
“Cynthia <xin2604@gmail.com>
2022-01-20 20:13:20 -0800

    add cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const router = require('./router.js');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
@@ -9,7 +10,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use(cors());
 app.use('/api', router);
 
 app.listen(port, (err) => {
